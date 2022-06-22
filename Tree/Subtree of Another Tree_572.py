27e30880640972645cccf6e81d1a40ada2edf974
Pritam Deb <deb.pritam95@gmail.com>
2022-06-22 11:42:54 +0530

    Create Subtree of Another Tree_572.py

---
+++ Tree/Subtree of Another Tree_572.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        if not subRoot:
+            return True
+        if not root:
+            return False
+        
+        if self.sametree(root,subRoot):
+            return True
+        return (self.isSubtree(root.left,subRoot) or self.isSubtree(root.right,subRoot))
+        
+    def sametree(self,root,subRoot):
+        if not root and not subRoot:
+            return True
+        if root and subRoot and root.val == subRoot.val:
+            return (self.sametree(root.left,subRoot.left) and self.sametree(root.right,subRoot.right))
+        return False
