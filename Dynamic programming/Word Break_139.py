fdeea2100a05fa43c1f299f22afd3c38959ae57b
Pritam Deb <deb.pritam95@gmail.com>
2022-06-26 05:17:08 +0530

    Create Word Break_139.py

---
+++ Dynamic programming/Word Break_139.py
@@ -0,0 +1,25 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        def wordBreakRecur(s: str, word_dict: Set[str], start: int):
+            if start == len(s):
+                return True
+            for end in range(start + 1, len(s) + 1):
+                if s[start:end] in word_dict and wordBreakRecur(s, word_dict, end):
+                    return True
+            return False
+ 
+        return wordBreakRecur(s, set(wordDict), 0)
+  -----------------------------------------------------------------------------------------------------------------
+  class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        word_set = set(wordDict)
+        dp = [False] * (len(s) + 1)
+        dp[0] = True
+        
+        for i in range(1,len(s)+1):
+            for j in range(i):
+                if dp[j] and s[j:i] in word_set:
+                    dp[i] = True
+                    break
+        return dp[len(s)]
+                
