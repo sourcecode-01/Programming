d1ec4047cbb25ff8e50b137f6673c0ed1c5e25ef
Pritam Deb <deb.pritam95@gmail.com>
2022-06-30 01:13:26 +0530

    Update Longest Palindromic Substring_5.py

--- Dynamic programming/Longest Palindromic Substring_5.py
+++ Dynamic programming/Longest Palindromic Substring_5.py
@@ -1,20 +1,27 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        rev = s[::1]
-        m = len(s)
-        n = len(rev)
-        res = 0
-        
-        L = [[None]* (n+1) for i in range(m+1)]
-        
-        for i in range(m+1):
-            for j in range(n+1):
-                if i == 0 or j == 0:
-                    L[i][j] = 0
-                elif s[i-1] == rev[j-1]:
-                    L[i][j] = L[i-1][j-1] + 1
-                    res = max(res,L[i][j])
-                else:
-                    L[i][j] = 0
-                    
+        res = ""
+            
+        for i in range(len(s)):
+            
+            temp = validString(i, i, s)
+            if len(temp) > len(res):
+                res = temp
+            
+            temp = validString(i, i + 1, s)
+            if len(temp) > len(res):
+                res = temp
+                
         return res
+            
+    
+def validString(left, right, s):
+    left_p = left
+    right_p = right
+
+    while left_p >= 0 and right_p < len(s) and s[left_p] == s[right_p]:
+
+        left_p -= 1
+        right_p += 1
+        
+    return s[left_p+1:right_p] 
